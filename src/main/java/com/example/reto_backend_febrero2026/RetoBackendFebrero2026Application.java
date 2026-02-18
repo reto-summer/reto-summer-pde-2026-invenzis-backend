596fe9d40997eda6343964005d3414f37784ff13
NicooGon <nicolasd.gonzalez@correo.ucu.edu.uy>
2026-02-18 19:28:49 -0300

    Spring project

---
+++ src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
@@ -0,0 +1,13 @@
+package com.example.reto_backend_febrero2026;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetoBackendFebrero2026Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetoBackendFebrero2026Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reto_backend_febrero2026;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetoBackendFebrero2026ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
