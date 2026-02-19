8ccc8f5215abeadf5d1f61d4e51d2693eb0a821a
EnzoAparicio <enzuii@hotmail.com>
2026-02-19 20:57:23 -0300

    mail service, scheduler & thymeleaf template

--- src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
+++ src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
@@ -2,12 +2,13 @@ package com.example.reto_backend_febrero2026;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RetoBackendFebrero2026Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RetoBackendFebrero2026Application.class, args);
 	}
-
 }
