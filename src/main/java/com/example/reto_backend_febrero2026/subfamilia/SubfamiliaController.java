5b70b2beadb68c96678089b46ad46d56f2daf851
mikadominguezz <mikadominguezz2004@gmail.com>
2026-02-20 10:36:48 -0300

    controllers hechos de familia y subfamilia

--- src/main/java/com/example/reto_backend_febrero2026/subfamilia/SubfamiliaController.java
+++ src/main/java/com/example/reto_backend_febrero2026/subfamilia/SubfamiliaController.java
@@ -1,4 +1,35 @@
 package com.example.reto_backend_febrero2026.subfamilia;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/subfamilias")
+@CrossOrigin(origins = "*")
 public class SubfamiliaController {
+
+    private final ISubfamiliaService subfamiliaService;
+
+    public SubfamiliaController(ISubfamiliaService subfamiliaService) {
+        this.subfamiliaService = subfamiliaService;
+    }
+
+    @GetMapping
+    public List<SubfamiliaModel> findAll() {
+        return subfamiliaService.findAll();
+    }
+
+    @GetMapping("/familia/{famiCod}")
+    public List<SubfamiliaModel> findByFamiCod(@PathVariable Integer famiCod) {
+        return subfamiliaService.findByFamiCod(famiCod);
+    }
+
+    @GetMapping("/{famiCod}/{cod}")
+    public SubfamiliaModel findById(
+            @PathVariable Integer famiCod,
+            @PathVariable Integer cod) {
+
+        return subfamiliaService.findById(famiCod, cod);
+    }
 }
