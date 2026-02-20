e02319455fa6454ee0214e64857dcd2b491784d4
Mauro Barrales <bmauro25@gmail.com>
2026-02-19 22:44:38 -0300

    Add ARCE RSS integration: DTOs, RestClient service and JacksonXML config

--- src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
+++ src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
@@ -2,8 +2,13 @@ package com.example.reto_backend_febrero2026;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+})
 public class RetoBackendFebrero2026Application {
 
 	public static void main(String[] args) {
