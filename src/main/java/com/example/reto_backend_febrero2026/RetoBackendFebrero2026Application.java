3b4137c5b73debb34eabe896a7fb0470c4e6fc98
enzui <enzo.aparicio@correo.ucu.edu.uy>
2026-02-20 10:09:57 -0300

    fix licitacionModel y mail-properties

--- src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
+++ src/main/java/com/example/reto_backend_febrero2026/RetoBackendFebrero2026Application.java
@@ -2,6 +2,7 @@ package com.example.reto_backend_febrero2026;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
