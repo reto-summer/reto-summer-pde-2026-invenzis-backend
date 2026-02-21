30e434fb2ec4c4a31f0ab808f5368dacee4ae019
AgustinG130 <AGUSTINANDRES.GARCIA@CORREO.UCU.EDU.UY>
2026-02-20 22:04:05 -0300

    licitacion estructura

---
+++ src/main/java/com/example/reto_backend_febrero2026/licitacion/controller/LicitacionController.java
@@ -0,0 +1,52 @@
+package com.example.reto_backend_febrero2026.licitacion.controller;
+
+import com.example.reto_backend_febrero2026.licitacion.dto.LicitacionModelDTO;
+import com.example.reto_backend_febrero2026.licitacion.service.interfaces.ILicitacionService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@RestController
+    @RequestMapping("/licitaciones")
+    public class LicitacionController {
+
+        @Autowired
+        private ILicitacionService tenderService;
+        /*
+        @GetMapping("/ById/{id}")
+        public ResponseEntity<LicitacionModelDTO> getTenderById(@PathVariable int id) {
+            LicitacionModelDTO tender = tenderService.getTenderById(id);
+            return ResponseEntity.ok(tender);
+        }
+
+        @GetMapping("/title/{titulo}")
+        public ResponseEntity<LicitacionModelDTO> getTenderByTitle(@PathVariable String titulo) {
+            LicitacionModelDTO tender = tenderService.getTenderByTitle(titulo);
+            return ResponseEntity.ok(tender);
+        }
+
+        @GetMapping("/fecha_publicacion/{YYYY-MM-DD}")
+        public ResponseEntity<LicitacionModelDTO> getTenderByfecha_publicacion(@PathVariable LocalDate fecha) {
+            LicitacionModelDTO tender = tenderService.getTenderByfecha_publicacion(fecha);
+            return ResponseEntity.ok(tender);
+        }
+
+        @GetMapping("/fecha_cierre/{YYYY-MM-DDTHH:MM:SS}")
+        public ResponseEntity<LicitacionModelDTO> getTenderByfecha_cierre(@PathVariable LocalDateTime fecha) {
+            LicitacionModelDTO tender = tenderService.getTenderByfecha_cierre(fecha);
+            return ResponseEntity.ok(tender);
+        }
+
+        @GetMapping("/fecha_publicacion/desde/{YYYY-MM-DD}/hasta/{YYYY-MM-SS}")
+        public ResponseEntity<LicitacionModelDTO> getTenderByfecha_inicio_fin(@PathVariable LocalDate fecha_inicio, @PathVariable LocalDate fecha_fin) {
+            LicitacionModelDTO tender = tenderService.getTenderByfecha_inicio_fin(fecha_inicio, fecha_fin);
+            return ResponseEntity.ok(tender);
+        }
+        */
+    }
