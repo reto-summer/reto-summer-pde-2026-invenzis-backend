99213c411fd69b3709ae14f54bc284f35a204020
NicooGon <nicolasd.gonzalez@correo.ucu.edu.uy>
2026-02-24 12:05:12 -0300

    get all licitaciones

--- src/main/java/com/example/reto_backend_febrero2026/licitacion/ILicitacionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.reto_backend_febrero2026.licitacion;
-
-public interface ILicitacionRepository {
-    Licitacion getLicitacionById(int licitacionId);
-    Licitacion save(Licitacion licitacion);
-    Licitacion getLicitacionByTitulo(String titulo);
-}
