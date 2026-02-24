0b465019a1a1f979caf6c532b723a75f8aca20d4
Mauro Barrales <bmauro25@gmail.com>
2026-02-24 01:37:31 -0300

    feature/dev-cleanup: Code Structure changes, and licitaciones endpoint working READY TO DEV

---
+++ src/main/java/com/example/reto_backend_febrero2026/subfamilia/ISubfamiliaService.java
@@ -0,0 +1,16 @@
+package com.example.reto_backend_febrero2026.subfamilia;
+
+import java.util.List;
+
+public interface ISubfamiliaService {
+
+    List<SubfamiliaDTO> findAll();
+
+    SubfamiliaDTO findById(Integer famiCod, Integer cod);
+
+    List<SubfamiliaDTO> findByFamiCod(Integer famiCod);
+
+    SubfamiliaDTO saveFamily(SubfamiliaDTO subFamilia);
+
+    SubfamiliaDTO getOrCreateSubFamily(Integer famCod, Integer subCod);
+}
