95b5ff4e19e081800ff837f94b3e88650b6a9d5d
ignaciordgz <IGNACIOE.RODRIGUEZ@CORREO.UCU.EDU.UY>
2026-02-25 01:50:55 -0300

    Cron on LicitacionScheduler & Config

--- src/main/java/com/example/reto_backend_febrero2026/licitacion/LicitacionScheduler.java
+++ src/main/java/com/example/reto_backend_febrero2026/licitacion/LicitacionScheduler.java
@@ -1,9 +1,12 @@
 package com.example.reto_backend_febrero2026.licitacion;
 
+import com.example.reto_backend_febrero2026.config.Config;
+import com.example.reto_backend_febrero2026.config.IConfigService;
 import com.example.reto_backend_febrero2026.integration.servlet.service.ArceClientService;
 import com.example.reto_backend_febrero2026.mail.MailService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,11 +16,24 @@ public class LicitacionScheduler {
 
     private final ArceClientService arceClientService;
     private final MailService mailService;
+    private final IConfigService configService;
 
-    public LicitacionScheduler(ArceClientService arceClientService, MailService mailService) {
+    public LicitacionScheduler(ArceClientService arceClientService, MailService mailService, IConfigService configService) {
         this.arceClientService = arceClientService;
         this.mailService = mailService;
+        this.configService = configService;
     }
+
+    @Scheduled(cron = "0 0 0 * * *", zone = "America/Montevideo")
+    public void getLicitacionesByConfig()
+    {
+        Config config = this.configService.getConfig();
+
+        System.out.println(config.getFamilia().getCod());
+        System.out.println(config.getSubfamilia().getCod());
+        this.arceClientService.obtenerLicitaciones(3, 10);
+    }
+
 
     /*
     @Scheduled(cron = "0 * * * * *")
